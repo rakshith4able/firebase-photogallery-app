6042dbe702d1ec02e901cc387a02d9e2e29f177f
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2021-10-25 21:39:17 +0530

    Installed firebase and configured firestore and storage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import Header from './components/Header';
-import './App.css';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import 'firebase/storage';
+import 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
-}
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBysyK12fCQrXVyGQ-vm0go9NKFGLwD4NA",
+  authDomain: "fire-gallery-48321.firebaseapp.com",
+  projectId: "fire-gallery-48321",
+  storageBucket: "fire-gallery-48321.appspot.com",
+  messagingSenderId: "610141016239",
+  appId: "1:610141016239:web:870ea728b1de3d59da445a"
+};
 
-export default App;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const projectStorage = getStorage(app);
+
+const projectFirestore = getFirestore(app);
+
+export { projectStorage, projectFirestore };
